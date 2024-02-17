1d2e1508c681fe97f4f2085aac670cc5d8a4d18b
Matthew Bacarro <mbacarro@uw.edu>
2024-02-16 18:01:23 -0800

    finished basic navbar

---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+const Dropdown = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOptionClick = (option) => {
+        props.onSelect(option);
+        setIsOpen(false);
+    };
+
+    const handleMouseLeave = () => {
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="relative inline-block mx-5 my-3 text-left" >
+            <div>
+                <button
+                    type="button"
+                    className="inline-flex justify-between w-full px-4 py-2 text-base font-medium leading-5 text-gray-700 bg-white "
+                    onClick={() => setIsOpen(!isOpen)}
+                    aria-haspopup="true"
+                    aria-expanded="true"
+                >
+                    {props.buttonText}
+                    <svg
+                        className="w-5 h-5 ml-2 -mr-1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M10 12a1 1 0 0 1-.707-.293l-4-4a1 1 0 1 1 1.414-1.414L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-.707.293z"
+                        />
+                    </svg>
+                </button>
+            </div>
+            {isOpen && (
+                <div className="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg" onMouseLeave={handleMouseLeave}> 
+                    <div className="bg-white rounded-md shadow-xs">
+                        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                            {props.options.map((option) => (
+                                <div
+                                    key={option}
+                                    className="block px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-100 hover:text-gray-900"
+                                    onClick={() => handleOptionClick(option)}
+                                    role="menuitem"
+                                >
+                                    {option}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Dropdown;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,30 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
+import Dropdown from '../Dropdown/Dropdown';
 
 export default function NavBar(props) {
 
     return (
         <header>
             <nav>
-                <div className='flex my-2.5'>
-                    <div id="search" className='flex items-center justify-center w-[200px] h-8 ml-10 mr-auto border border-black border-solid justify-items-center'>
+                {/* Top Header */}
+                <div className='flex my-2.5 px-10 shadow-lg'>
+                    <Link to="/" className="text-[42px] font-bold text-violet-700 mr-8 my-auto">UCycle</Link>
+
+                    <div id="search" className='flex items-center justify-center h-10 my-auto mr-auto border border-black border-solid rounded-full w-52 justify-items-center'>
                         <p>temp search bar</p>
                     </div>
-                    <div id="logo">
-                        <Link to="/" className="text-2xl font-bold text-violet-700">UCycle</Link>
-                    </div>
-                    <div id="actions" className='flex items-center justify-center ml-auto'>
-                        <button class="h-8 w-28 text-white text-center bg-violet-700 rounded-md hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 dark:bg-violet-600 dark:hover:bg-violet-700 focus:outline-none dark:focus:ring-violet-800">
+                    <div id="actions" className='flex items-center justify-center my-auto ml-auto'>
+                        <button class="h-10 w-32 text-white text-center text-xl bg-violet-700 rounded-full hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 dark:bg-violet-600 dark:hover:bg-violet-700 focus:outline-none dark:focus:ring-violet-800">
                             Sell
                         </button>
-                        <div className='flex gap-4 ml-4'>
+                        <div className='flex gap-4 ml-8'>
                             <div className='w-6 h-6 border border-black border-solid'></div>
                             <div className='w-6 h-6 border border-black border-solid'></div>
                             <div className='w-6 h-6 border border-black border-solid'></div>
                             <div className='w-6 h-6 border border-black border-solid'></div>
                         </div>
-
                     </div>
+                </div>
+                {/* Bottom Header */}
+                <div className='flex justify-center'>
+                    <Dropdown 
+                        buttonText="Womenswear"
+                        options={["option 1", "option 2"]} 
+                    />
+                    <Dropdown
+                        buttonText="Menswear"
+                        options={["option 1", "option 2"]}
+                    />
+                    <Dropdown
+                        buttonText="Home Goods"
+                        options={["option 1", "option 2"]}
+                    />
+                    <Dropdown
+                        buttonText="Electronics"
+                        options={["option 1", "option 2"]}
+                    />
+                    <Dropdown
+                        buttonText="Accessories"
+                        options={["option 1", "option 2"]}
+                    />
+                    <Dropdown
+                        buttonText="Misc"
+                        options={["option 1", "option 2"]}
+                    />
                 </div>
             </nav>
         </header>
