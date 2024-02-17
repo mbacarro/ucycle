f5b76fd38bfdab506b18ba9cf26a2583624443bd
Matthew Bacarro <mbacarro@uw.edu>
2024-02-16 19:45:17 -0800

    finished home page mockup

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,10 +6,76 @@ export default  function Home(props) {
     return (
         <>
             <NavBar />
-            <div className="flex items-center justify-center h-screen bg-gray-100">
-                <div className="p-6 bg-white rounded shadow-lg">
-                    <h1 className="mb-4 text-2xl font-bold">Home Page</h1>
-                    <p className="text-gray-700">This is a simple homepage created using React and Tailwind CSS.</p>
+            <div className='flex flex-col gap-20 mx-20 my-20'>
+                {/* Necessities */}
+                <div id="Necessities" className='flex justify-center gap-8 '>
+                    <div id='Apartment Necessities' className='p-4 flex flex-col w-1/3 aspect-[13/10] bg-gray-400'>
+                        <h1 className='w-1/4 text-2xl font-bold text-white'>Apartment Necessities</h1>
+                        <button className='self-center w-1/4 px-2 py-2 mt-auto mb-4 text-base text-center bg-white border-2 border-black rounded-md'>Shop Now</button>
+                    </div>
+                    <div id='Dorm Necessities' className='p-4 flex flex-col w-1/3 aspect-[13/10] bg-gray-400'>
+                        <h1 className='w-1/4 text-2xl font-bold text-white'>Dorm Necessities</h1>
+                        <button className='self-center w-1/4 px-2 py-2 mt-auto mb-4 text-base text-center bg-white border-2 border-black rounded-md'>Shop Now</button>
+                    </div>
+                </div>
+
+                {/* Shop by Categories */}
+                <div id='Categories' className='flex flex-col justify-center gap-6'>
+                    <h1 className='text-2xl font-bold text-black'>Shop by Category</h1>
+                    <div className='flex gap-8'>
+                        <div id='Womenswear' className='flex flex-col w-1/6 aspect-square'>
+                            <div id='Womenswear Img' className='flex w-full border border-red aspect-square'>
+
+                            </div>
+                            <h2 className='mt-3 text-lg font-medium text-black'>Womenswear</h2>
+                        </div>
+                        <div id='Menswear' className='flex flex-col w-1/6 aspect-square'>
+                            <div id='Menswear Img' className='flex w-full border border-red aspect-square'>
+
+                            </div>
+                            <h2 className='mt-3 text-lg font-medium text-black'>Menswear</h2>
+                        </div>
+                        <div id='Home Goods' className='flex flex-col w-1/6 aspect-square'>
+                            <div id='Home Goods Img' className='flex w-full border border-red aspect-square'>
+
+                            </div>
+                            <h2 className='mt-3 text-lg font-medium text-black'>Home Goods</h2>
+                        </div>
+                        <div id='Electronics' className='flex flex-col w-1/6 aspect-square'>
+                            <div id='Electronics Img' className='flex w-full border border-red aspect-square'>
+
+                            </div>
+                            <h2 className='mt-3 text-lg font-medium text-black'>Electronics</h2>
+                        </div>
+                        <div id='Accessories' className='flex flex-col w-1/6 aspect-square'>
+                            <div id='Accessories Img' className='flex w-full border border-red aspect-square'>
+
+                            </div>
+                            <h2 className='mt-3 text-lg font-medium text-black'>Accessories</h2>
+                        </div>
+                        <div id='Misc' className='flex flex-col w-1/6 aspect-square'>
+                            <div id='Misc Img' className='flex w-full border border-red aspect-square'>
+
+                            </div>
+                            <h2 className='mt-3 text-lg font-medium text-black'>Misc</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Popular Itme */}
+                <div id='Categories' className='flex flex-col gap-6 justify-evenly'>
+                    <h1 className='text-2xl font-bold text-black'>Popular Items</h1>
+                    <div className='grid grid-cols-4 gap-10'>
+                        {[...Array(10).keys()].map((i) => (
+                            <div key={`Item${i + 1}`} id={`Item${i + 1}`} className='flex flex-col w-full aspect-square'>
+                                <div id={`Item${i + 1}Img`} className='flex w-full border border-red aspect-square'>
+                                    {/* Placeholder for item image */}
+                                </div>
+                                <h2 className='mt-3 text-lg font-medium text-black'>Item {i + 1}</h2>
+                                <h3 className='mt-3 font-normal text-black text-mg'>$ Price</h3>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
