ae16376d05e24c24ba9a042c9f75f9e0e18e273a
Matthew Bacarro <mbacarro@uw.edu>
2024-04-29 14:46:19 -0700

    updated itemdetails page with updated figma

--- frontend/src/Pages/ItemDetails/ItemDetails.js
+++ frontend/src/Pages/ItemDetails/ItemDetails.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import { items } from '../../SampleInventory/sampleInventory';
 
 import NavBar from '../../Components/Navbar/Navbar';
 import SchoolIcon from '../../Images/SchoolIcon.svg'
@@ -66,19 +65,46 @@ export default function ItemDetails() {
                     <div className='w-1/2 h-fit'>
                         {/* General Item Info*/}
                         <div className='mx-2.5 mb-2'>
-                            <h1 className='text-2xl font-semibold'>{listing.name}</h1>
-                            <p className='mb-3 text-lg'>${listing.price}</p>
-                            <p className='mb-3 text-base'>{listing.description}</p>
-                            <div className='flex gap-3 mb-8'>
+                            <h1 className='mb-2 text-2xl font-semibold '>{listing.name}</h1>
+                            <p className='mb-6 text-lg'>${listing.price}</p>
+                            {/* <div className='flex gap-3 mb-8'>
+                                <div className='w-8 h-8 rounded-full bg-slate-600'></div>
                                 <div className='w-8 h-8 rounded-full'></div>
-                                <div className='w-8 h-8 rounded-full'></div>
-                            </div>
+                            </div> */}
                             <div className='flex gap-3'>
-                                <button className="px-10 py-2 text-xl font-semibold text-center text-white border rounded h-fit w-fit bg-violet-700 hover:bg-violet-800">Add to bag</button>
-                                <button className="px-10 py-2 text-xl font-semibold text-center text-black border border-black rounded h-fit w-fit">Report Item</button>
+                                <button 
+                                    className="px-10 py-2 text-lg font-semibold text-center text-white border rounded h-fit w-fit bg-violet-700 hover:bg-violet-800"
+                                >
+                                    Buy for ${listing.price}
+                                </button>
+                                <button 
+                                    className="px-10 py-2 text-lg font-semibold text-center border rounded border-violet-700 text-violet-700 h-fit w-fit"
+                                >
+                                    Make an offer
+                                </button>
                             </div>
                         </div>
+                        
                         <hr className='my-8 border mx-2.5'/>
+
+                        <div className='mx-2.5 mb-2'>
+                            <h2 className='mb-4 text-xl font-semibold text-black mt-9'>Details</h2>
+                            <div class="flex gap-12 mb-6">
+                                <div class="text-neutral-500 text-base font-medium flex flex-col gap-2">
+                                    <p>Condition: <span id="condition"></span></p>
+                                    <p>Category: <span id="category"></span></p>
+                                </div>
+                                <div className='flex flex-col gap-2 text-base font-normal text-black'>
+                                    <p> {listing.condition}</p>
+                                    <p> {listing.category}</p>
+                                </div>
+                            </div>
+                            <h2 className='mb-4 text-xl font-semibold text-black mt-9'>Description</h2>
+                            <p className='mb-4 text-base'>{listing.description}</p>
+                        </div>
+                        
+                        <hr className='my-8 border mx-2.5' />
+
                         {/* Pickup Options*/}
                         <div className='mx-2.5 mb-2 '>
                             <h2 className='mb-4 text-lg font-semibold'>Pick-up Options</h2>
@@ -104,13 +130,19 @@ export default function ItemDetails() {
                         <div className='mx-2.5 mb-2 '>
                             <h2 className='mb-4 text-lg font-semibold'>Seller Information</h2>
                             <div className='flex gap-3 mx-2 my-9'>
-                                <div className='border border-black h-14 w-14'>Profile Img</div>
+                                <img
+                                    className='object-cover rounded-full w-14 h-14'
+                                    alt='Tailwind CSS chat bubble component'
+                                    src={
+                                        "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+                                    }
+                                />
                                 <div className='flex flex-col'>
                                     <h3 className='text-lg font-semibold'>{listing.sellerID}</h3>
                                     <div>Rating Placeholder</div>
                                 </div>
                             </div>
-                            <button className="w-2/3 px-10 py-2 text-base font-semibold text-center text-black border border-black rounded h-fit">Message seller</button>
+                            <button className="w-2/3 px-10 py-2 text-base font-semibold text-center text-white rounded bg-violet-700 h-fit">Message seller</button>
 
                         </div>
                     </div>
@@ -123,8 +155,6 @@ export default function ItemDetails() {
             </div>
             <div className='mx-40 my-20'>
                 <h2 className='text-2xl font-bold my-7'>More items from {listing.sellerID}'s Shop</h2>
-                <p >Condition: {listing.condition}</p>
-                <p >subcategory: {listing.subcategory}</p>
                 <p >Payment Methods: {listing.paymentMethod.join(', ')}</p>
             </div>
         </>
